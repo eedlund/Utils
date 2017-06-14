92f3a132dc0e0fc8a4771b8a1d9a017fb947ff65
edlunde-dnastar <edlunde@dnastar.com>
2017-06-14 15:38:41 -0700

    MMTF support
    Use latest BioJava Snapshot and support MMTF conversion

--- BioUtils/src/main/java/JavaBio/JavaBio/App.java
+++ BioUtils/src/main/java/JavaBio/JavaBio/App.java
@@ -3,50 +3,66 @@ package JavaBio.JavaBio;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
 
 import org.biojava.nbio.structure.Structure;
 import org.biojava.nbio.structure.StructureException;
 import org.biojava.nbio.structure.StructureIO;
-import org.biojava.nbio.structure.StructureIO.StructureFiletype;
 import org.biojava.nbio.structure.align.util.AtomCache;
+import org.biojava.nbio.structure.io.mmtf.MmtfActions;
+import org.biojava.nbio.structure.io.mmtf.MmtfUtils;
 
 /**
  *
  */
 public class App 
 {
-    public static void main( String[] args ) throws IOException, StructureException
-    {
-    	if ( args.length != 1 ){
-    		printHelpAndExit();
-    	}
-        File f = new File(args[0]);
-        if ( ! f.exists() ) {
-        	System.err.println("Unable to find file: " + f.getAbsolutePath());
-        	printHelpAndExit();
-        }
-        StructureFiletype type = StructureIO.guessFiletype(f.getAbsolutePath());
-        
-        AtomCache cache = new AtomCache();
-        
-        cache.setUseMmCif(type == StructureFiletype.PDB);
-        Structure s = StructureIO.getStructure(f.toURI().toURL().toString());
-        
-        String out = null;
-        String ext = "";
-        if ( type == StructureFiletype.PDB){
-        	out = s.toMMCIF();
-        	ext = ".cif";
-        }else{
-        	out = s.toPDB();
-        	ext = ".pdb";
-        }
-        File output = new File(f.getParent(), f.getName() + ext);
-        Files.write(output.toPath(), out.getBytes());
-    }
+	public static void main( String[] args ) throws IOException, StructureException
+	{
+		if ( args.length != 2 ){
+			printHelpAndExit();
+		}
+		File f = new File(args[0]);
+		if ( ! f.exists() ) {
+			System.err.println("Unable to find file: " + f.getAbsolutePath());
+			printHelpAndExit();
+		}
+		String outputFmt = args[1];
+		if ( !validOutputFormat(outputFmt) ){
+			System.err.println("Invalid output format: " + outputFmt);
+			printHelpAndExit();
+		}
+
+		AtomCache cache = new AtomCache();
+
+		Structure s = StructureIO.getStructure(f.toURI().toURL().toString());
+
+		File output = new File(f.getParent(), f.getName() + "." + outputFmt);
+		String out = null;
+
+		if ( outputFmt.equalsIgnoreCase("mmtf")){
+			MmtfUtils.setUpBioJava();
+			MmtfActions.writeToFile(s, output.toPath());
+		}else{
+			if ( outputFmt.equalsIgnoreCase("cif")){
+				out = s.toMMCIF();
+			}else{
+				out = s.toPDB();
+			}
+			Files.write(output.toPath(), out.getBytes());
+		}
+
+	}
+
+	private static boolean validOutputFormat(String outputFmt) {
+		List<String> validTypes = Arrays.asList(new String[]{"pdb", "cif", "mmtf"});
+		return validTypes.contains(outputFmt.toLowerCase());
+	}
 
 	private static void printHelpAndExit() {
-		System.err.println("Usage: java BioUtils.jar $FILE, where $FILE is a PDB or mmCIF file you wish to convert.");
+		System.err.println("Usage: java -jar BioUtils.jar $FILE $TYPE, where $FILE is a PDB or mmCIF file you wish to convert and " +
+				"$TYPE is the type of the output file [PDB, CIF, MMTF].");
 		System.exit(1);
 	}
 }
